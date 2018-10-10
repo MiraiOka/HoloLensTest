2db0a81861c0f5d0cd84468d2f178f6a9c25b56d
MiraOka <uaofmsbct.9g@gmail.com>
2018-10-11 08:59:55 +0900

    debug ray by spheres

--- Assets/Developments/Scripts/PositionController.cs
+++ Assets/Developments/Scripts/PositionController.cs
@@ -9,6 +9,18 @@ public class PositionController : MonoBehaviour {
 
     [SerializeField] Text text;
     [SerializeField] Camera holoCamera;
+    [SerializeField] GameObject raySpheres;
+    [SerializeField] GameObject spherePrefab;
+    [SerializeField] float length = 10; //会場のスケール
+    [SerializeField] GameObject panel;
+
+    public enum Target
+    {
+        first,
+        second,
+        third
+    }
+    Target target = Target.first;
 
     private void Start()
     {
@@ -19,7 +31,6 @@ public class PositionController : MonoBehaviour {
 
     void InteractionSourcePressed(InteractionSourcePressedEventArgs ev)
     {
-        text.text = "tap!!!";
 
         //Vector3 headPos = Camera.main.transform.position;
         //Vector3 direction = Camera.main.transform.forward;
@@ -29,7 +40,37 @@ public class PositionController : MonoBehaviour {
 
         //Ray ray = new Ray(headPos, direction);
         Debug.DrawRay(headPos, direction, Color.yellow, 100, false);
-        Debug.Log(headPos);
-        Debug.Log(direction);
+        
+
+        raySpheres.transform.position = headPos;
+
+        switch(target)
+        {
+            case Target.first:
+                for (int i = 0; i < length * 10; i++)
+                {
+                    GameObject sphere = Instantiate(spherePrefab, raySpheres.transform.position + direction * (i / 10.0f), Quaternion.identity);
+                    sphere.transform.parent = raySpheres.transform;
+                }
+
+                text.text = "ムックをクリックしてください。";
+                target = Target.second;
+                break;
+            case Target.second:
+
+
+
+
+                text.text = "ガチャピンをクリックしてください。";
+                target = Target.third;
+                break;
+            case Target.third:
+
+                Destroy(panel);
+                break;
+            default:
+                break;
+        }
+        
     }
 }
